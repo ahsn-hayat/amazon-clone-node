d7a9afa28997b758df4ecabcfcba409aa91495a3
Ali-Khawar <35522871+ahsn-hayat@users.noreply.github.com>
2023-06-28 18:13:56 +0500

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,14 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const auth = async (req, res, next) => {
   try {
+    // const authHeader = req.header("Authorization");
+    // if (!authHeader || !authHeader.startsWith("Bearer "))
+    //   return res.status(401).json({ msg: "No auth token, access denied!" });
+
+    // const token = authHeader.substring(7);
+
     const token = req.header("x-auth-token");
     if (!token)
       return res.status(401).json({ msg: "No auth token, access denied!" });
@@ -17,9 +24,7 @@ const auth = async (req, res, next) => {
     if (!user) return res.status(401).json({ msg: "User not found" });
 
     if (user.type != "user")
-      return res
-        .status(401)
-        .json({ msg: "User can not perform this action." });
+      return res.status(401).json({ msg: "User can not perform this action." });
 
     req.user = verified.id;
     req.token = token;
